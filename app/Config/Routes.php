2094af43738e6fc3610d019dfe5d7effa90d4ed6
RidhoAmanullah-19221112 <145321609+RidhoAmanullah-19221112@users.noreply.github.com>
2024-01-12 20:55:21 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,7 +9,8 @@ $routes->get('/', 'Home::dashboardp');
 
 $routes->get('/LoginView', 'LoginController::form');
 $routes->post('/LoginView', 'LoginController::login');
-$routes->get('/logout', 'LoginController::logout');
+$routes->get('/keluar', 'LoginController::logout');
+$routes->post('/keluar', 'LoginController::logout');
 
 $routes->get('/Dashboard/Siswa', 'SiswaController::tampil_dashboard');
 $routes->get('/Dashboard/Pengajar', 'PengajarController::tampil_dashboard');
@@ -30,5 +31,15 @@ $routes->post('/Dashboard/Pengajar/Daftar-Siswa', 'DaftarsiswaController::tampil
 $routes->get('/Dashboard/Pengajar/Nilai-Siswa', 'NilaiController::tampilpengajar');
 $routes->post('/Dashboard/Pengajar/Nilai-Siswa', 'NilaiController::tampilpengajar');
 
-$routes->get('/Dashboard/index.php', 'PengajarController::absensi');
-$routes->get('/Dashboard/tampildata.php', 'MateriController::tampil');
+$routes->get('/Dashboard/Siswa/Home', 'SiswaController::tampil_dashboard');
+$routes->post('/Dashboard/Siswa/Home', 'SiswaController::tampil_dashboard');
+$routes->get('/Dashboard/Siswa/Absensi', 'AbsensiController::absensisiswa');
+$routes->post('/Dashboard/Siswa/Absensi', 'AbsensiController::absensisiswa');
+$routes->get('/Dashboard/Siswa/Nilai-Siswa', 'NilaiController::tampilsiswa');
+$routes->post('/Dashboard/Siswa/Nilai-Siswa', 'NilaiController::tampilsiswa');
+$routes->get('/Dashboard/Siswa/Materi-Siswa', 'MateriController::tampilsiswa');
+$routes->post('/Dashboard/Siswa/Materi-Siswa', 'MateriController::tampilsiswa');
+$routes->get('/Dashboard/Siswa/Kelas', 'KelasController::tampilsiswa');
+$routes->post('/Dashboard/Siswa/Kelas', 'KelasController::tampilsiswa');
+$routes->get('/Dashboard/Siswa/Jadwal', 'JadwalController::tampilsiswa');
+$routes->post('/Dashboard/Siswa/Jadwal', 'JadwalController::tampilsiswa');
